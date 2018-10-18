5d33409ea62d00dee10e8cb90fb6f0408a793ac6
paul <sl.zhou@upltv.com>
2018-10-18 14:08:16 +0800

    同步

--- source/conf.py
+++ source/conf.py
@@ -84,7 +84,8 @@ pygments_style = None
 #
 html_theme = 'sphinx_rtd_theme'
 def setup(app):
- app.add_stylesheet("css/main.css" )
+ app.add_stylesheet("css/custom.css" )
+ app.add_javascript('js/custom.js')
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
